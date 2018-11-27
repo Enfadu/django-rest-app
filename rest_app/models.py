da5730c7ec0bdb1e89744ea2cd54d05458577bbd
Ridge Coffman <enfaduraa+github@Gmail.com>
2018-11-27 12:05:42 -0500

    Remove clean function

--- rest_app/models.py
+++ rest_app/models.py
@@ -7,9 +7,5 @@ class NameModel(models.Model):
     name = models.CharField(max_length=140)
     email = models.EmailField(max_length=140)
 
-    #def clean(str):
-    #    all_clean_data = super().clean()
-    #    email = all_clean_data['email']
-
     def __str__(self):
         return self.name
